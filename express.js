33d5a848e4fe5d7c3fa8a3cd637431412ffcd3f1
Aaron Chau <aaronchauchau@gmail.com>
2016-10-09 00:28:00 -0700

    Update express.js

--- express.js
+++ express.js
@@ -3,9 +3,9 @@ var Webtask    = require('webtask-tools');
 var bodyParser = require('body-parser');
 var app = express();
 
-module.exports = function(callback){
-  // Prints the message "Hello Webtask" when accessed
-  callback(null, "Hello Webtask");
+module.exports = function(context, request, response){
+  response.writeHead(200, { 'Content-Type': 'text/html '});
+  response.end('<h1>Hello Webtask!</h1>');
 }
 
 module.exports = Webtask.fromExpress(app);
