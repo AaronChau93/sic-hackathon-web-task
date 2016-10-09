80c2f0d8ff210e20c88d72bdd9f1066fac394cd5
Aaron Chau <aaronchauchau@gmail.com>
2016-10-09 00:00:05 -0700

    Update express.js

--- express.js
+++ express.js
@@ -2,7 +2,7 @@ var express    = require('express');
 var Webtask    = require('webtask-tools');
 var bodyParser = require('body-parser');
 var app = express();
-
+var port = process.env.PORT || 3000;
 var http = require('http').Server(app);
 var io = require('socket.io');
 var socket = io(http);
@@ -10,8 +10,9 @@ var socket = io(http);
 var BASE_URL = "https://api.kandy.io/";
 
 app.use(bodyParser.json());
-
-
+http.listen(port, function() {
+  console.log("listening ");
+});
 socket.on('connection', function(client){
   console.log('someone connected to namespace ');
  
