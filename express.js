9d3814b5e202741a7db038c8ed2fcb44cf46193a
Aaron Chau <aaronchauchau@gmail.com>
2016-10-08 20:42:10 -0700

    testing post

--- express.js
+++ express.js
@@ -11,12 +11,12 @@ app.use(bodyParser.json());
 var account = {access_token : null};
 var restAcessToken = "/v1.2/accounts/accesstokens?key=AAKa2552b36872e464fa10f79fb5a3153a9&account_api_secret=AAS947d72261ead4ad4adfa6048cdd96a74";
 
-request(BASE_URL + restAcessToken, function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-    console.log(body);
-    account.access_token = JSON.parse(body).result.account_access_token;
-  } 
-});
+// request(BASE_URL + restAcessToken, function (error, response, body) {
+//   if (!error && response.statusCode == 200) {
+//     console.log(body);
+//     account.access_token = JSON.parse(body).result.account_access_token;
+//   } 
+// });
 function sendKandyMessageRequest(key, device_id, messageJSON) {
     var url_ext = "/v1.2/devices/messages?key=" + key + "&device_id=" + device_id;
     request.post(BASE_URL + url_ext,
